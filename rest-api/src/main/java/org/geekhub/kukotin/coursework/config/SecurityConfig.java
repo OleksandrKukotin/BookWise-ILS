53e55863956ff8c9a080b637e894a82dea16d63f
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-07-20 23:42:30 +0300

    Upgraded SecurityConfig

--- rest-api/src/main/java/org/geekhub/kukotin/coursework/config/SecurityConfig.java
+++ rest-api/src/main/java/org/geekhub/kukotin/coursework/config/SecurityConfig.java
@@ -2,15 +2,12 @@ package org.geekhub.kukotin.coursework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -18,49 +15,36 @@ import javax.sql.DataSource;
 public class SecurityConfig {
 
     private final PasswordEncoder encoder;
+    private final DataSource dataSource;
 
-    public SecurityConfig(PasswordEncoder encoder) {
+    public SecurityConfig(PasswordEncoder encoder, DataSource dataSource) {
         this.encoder = encoder;
+        this.dataSource = dataSource;
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, DataSource dataSource) throws Exception {
-        AuthenticationManagerBuilder auth = http.getSharedObject(AuthenticationManagerBuilder.class);
-
-        auth
-            .jdbcAuthentication()
-            .passwordEncoder(encoder)
-            .dataSource(dataSource)
-            .authoritiesByUsernameQuery("select authority from authorities when username = ?");
-
-        return auth.build();
-    }
-
-    @Bean
-    public UserDetailsManager users(DataSource dataSource) {
+    public UserDetailsManager users() {
         return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authRegistry -> authRegistry
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/api/**").permitAll()
-                .requestMatchers("/registration").anonymous()
-                .anyRequest().permitAll()
-            );
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        var authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
 
-        return http.build();
+        authenticationManagerBuilder
+            .jdbcAuthentication()
+            .withUser("admin")
+            .password("{bcrypt}$2a$10$rkWfnHrSpo0JyNBH4tHRDOeuZACtCU5v4sCQpleWl4P41YuYqQMjC") //admin
+            .roles("ADMIN");
+
+        authenticationManagerBuilder
+            .jdbcAuthentication()
+            .passwordEncoder(encoder)
+            .dataSource(dataSource)
+            .withUser("user")
+            .password("{bcrypt}$2a$10$GlpFG1Ml3U9AvkOu0D1B9ufnoquX5xqCR/NHaMfBZliYgPa8/e5sK")
+            .roles("USER");
+
+        return authenticationManagerBuilder.build();
     }
-
-    @Bean
-    public RoleHierarchy roleHierarchy() {
-        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-
-        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_LIBRARIAN > ROLE_USER > ROLE_GUEST");
-
-        return roleHierarchy;
-    }
-
 }
