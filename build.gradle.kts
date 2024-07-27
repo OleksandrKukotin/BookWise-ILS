38476f1ce3c42fc1e0b6acbde2fa8a78662dd65e
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-07-27 23:18:12 +0300

    Updated Spring Boot Starter version

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,18 +33,18 @@ allprojects {
 
     dependencies {
         implementation("com.google.code.findbugs:jsr305:3.0.2")
-        implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
-        implementation("org.springframework.boot:spring-boot-starter-jdbc:3.2.4")
-        implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
-        implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.2.4")
-        implementation("org.springframework.boot:spring-boot-starter-actuator:3.2.4")
+        implementation("org.springframework.boot:spring-boot-starter-web:3.3.2")
+        implementation("org.springframework.boot:spring-boot-starter-jdbc:3.3.2")
+        implementation("org.springframework.boot:spring-boot-starter-security:3.3.2")
+        implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.3.2")
+        implementation("org.springframework.boot:spring-boot-starter-actuator:3.3.2")
         implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
         implementation("org.postgresql:postgresql:42.7.3")
         implementation("org.flywaydb:flyway-core:10.7.1")
         runtimeOnly("org.flywaydb:flyway-database-postgresql:10.7.1")
 
-        testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.4")
+        testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.2")
         testImplementation("org.springframework.security:spring-security-test:6.2.4")
     }
 
