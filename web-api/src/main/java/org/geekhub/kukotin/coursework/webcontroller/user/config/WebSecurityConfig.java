2cffa86b263bc6d9e2d454a8bf0c773afc74de8c
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-07-31 23:53:25 +0300

    Added UserDetailService

--- web-api/src/main/java/org/geekhub/kukotin/coursework/webcontroller/user/config/WebSecurityConfig.java
+++ web-api/src/main/java/org/geekhub/kukotin/coursework/webcontroller/user/config/WebSecurityConfig.java
@@ -3,9 +3,20 @@ package org.geekhub.kukotin.coursework.webcontroller.user.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class WebSecurityConfig {
+
+    PasswordEncoder passwordEncoder;
+
+    public WebSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,5 +29,18 @@ public class WebSecurityConfig {
                 .permitAll())
             .logout(LogoutConfigurer::permitAll);
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+            User.builder()
+                .passwordEncoder(passwordEncoder::encode)
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
