cdb9d649910bd200e13747df05ddc7b5edc89962
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-08-07 23:30:13 +0300

    Created base User classes

---
+++ domain/src/main/java/org/geekhub/kukotin/coursework/service/user/User.java
@@ -0,0 +1,8 @@
+package org.geekhub.kukotin.coursework.service.user;
+
+public record User (
+    Long id,
+    String username,
+    String password
+){
+}
---
+++ persistence/src/main/java/org/geekhub/kukotin/coursework/repository/UserRepositoryImpl.java
@@ -0,0 +1,6 @@
+package org.geekhub.kukotin.coursework.repository;
+
+import org.geekhub.kukotin.coursework.service.user.UserRepository;
+
+public class UserRepositoryImpl implements UserRepository {
+}
