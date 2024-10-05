0d24d0d5044729b162965919575cd09865470921
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-10-05 23:14:45 +0300

    Created blank PostMapping for PasswordResetController

--- web-api/src/main/java/org/geekhub/kukotin/coursework/webcontroller/user/PasswordResetController.java
+++ web-api/src/main/java/org/geekhub/kukotin/coursework/webcontroller/user/PasswordResetController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -11,11 +13,38 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping("/passwordReset")
 public class PasswordResetController {
 
+//    private final UserService userService;
+//    private final EmailService emailService;
+//
+//    public PasswordResetController(UserService userService, EmailService emailService) {
+//        this.userService = userService;
+//        this.emailService = emailService;
+//    }
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public String passwordReset(Model model) {
         UserDTO userDTO = new UserDTO();
         model.addAttribute("user", userDTO);
+        return "forgotPasswordForm";
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String handlePasswordReset(@ModelAttribute("user") UserDTO userDTO, Model model) {
+//        String email = userDTO.getEmail();
+//        Optional<User> user = userService.findUserByEmail(email);
+//
+//        if (user.isPresent()) {
+//            String resetToken = UUID.randomUUID().toString();
+//            userService.createPasswordResetTokenForUser(user.get(), resetToken);
+//            emailService.sendSimpleMessage(email, "Password Reset", "Reset token: " + resetToken);
+//            return "resetPasswordConfirmation"; // Create a confirmation view
+//        } else {
+//            model.addAttribute("error", "User not found.");
+//            return "forgotPasswordForm";
+//        }
+        model.addAttribute("error", "Currently not supported");
         return "forgotPasswordForm";
     }
 }
