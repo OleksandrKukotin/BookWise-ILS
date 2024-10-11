b92d1820c15e2897193e4c95470c3e99d532f83a
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-10-11 23:47:55 +0300

    Upgraded UserDTO class

--- web-api/src/main/java/org/geekhub/kukotin/coursework/webcontroller/user/UserDTO.java
+++ web-api/src/main/java/org/geekhub/kukotin/coursework/webcontroller/user/UserDTO.java
@@ -1,18 +1,17 @@
 package org.geekhub.kukotin.coursework.webcontroller.user;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class UserDTO {
 
-    @NotNull
-    @NotEmpty
+    @NotEmpty(message = "Username can't be empty")
+    @Size(min = 5, max = 255)
     private String username;
-    @NotNull
-    @NotEmpty
+    @NotEmpty(message = "User's email can't be empty")
     private String email;
-    @NotNull
-    @NotEmpty
+    @NotEmpty(message = "User's password can't be empty")
+    @Size(min = 8)
     private String password;
     private String matchingPassword;
 
@@ -26,27 +25,27 @@ public class UserDTO {
     public UserDTO() {
     }
 
-    public @NotNull @NotEmpty String getUsername() {
+    public @NotEmpty(message = "Username can't be empty") @Size(min = 5, max = 255) String getUsername() {
         return username;
     }
 
-    public void setUsername(@NotNull @NotEmpty String username) {
+    public void setUsername(@NotEmpty(message = "Username can't be empty") @Size(min = 5, max = 255) String username) {
         this.username = username;
     }
 
-    public @NotNull @NotEmpty String getEmail() {
+    public @NotEmpty(message = "User's email can't be empty") String getEmail() {
         return email;
     }
 
-    public void setEmail(@NotNull @NotEmpty String email) {
+    public void setEmail(@NotEmpty(message = "User's email can't be empty") String email) {
         this.email = email;
     }
 
-    public @NotNull @NotEmpty String getPassword() {
+    public @NotEmpty(message = "User's password can't be empty") @Size(min = 8) String getPassword() {
         return password;
     }
 
-    public void setPassword(@NotNull @NotEmpty String password) {
+    public void setPassword(@NotEmpty(message = "User's password can't be empty") @Size(min = 8) String password) {
         this.password = password;
     }
 
