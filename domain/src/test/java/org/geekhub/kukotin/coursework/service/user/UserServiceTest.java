c326ce7bcd05651f5bd16dee151f8f02db8b23fc
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-11-05 23:35:52 +0200

    Created class for unit testing UserService

---
+++ domain/src/test/java/org/geekhub/kukotin/coursework/service/user/UserServiceTest.java
@@ -0,0 +1,49 @@
+package org.geekhub.kukotin.coursework.service.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserServiceTest {
+    // implement this
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getUsersPage() {
+    }
+
+    @Test
+    void getTotalUsersCount() {
+    }
+
+    @Test
+    void getUserByEmail() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void toggleUserStatus() {
+    }
+
+    @Test
+    void changeRole() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void getUserByUsername() {
+    }
+}
