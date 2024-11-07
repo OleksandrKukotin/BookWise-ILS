5792d54bc8a8bee51241b61122b117a18981e9bc
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-11-07 23:19:34 +0200

    Almost implemented several tests in UserServiceTest

--- domain/src/test/java/org/geekhub/kukotin/coursework/service/user/UserServiceTest.java
+++ domain/src/test/java/org/geekhub/kukotin/coursework/service/user/UserServiceTest.java
@@ -1,11 +1,18 @@
 package org.geekhub.kukotin.coursework.service.user;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
+    // realize how to use mocks and why they don't work in the following test cases
     @Mock
     private UserRepository userRepository;
 
@@ -21,10 +28,23 @@ class UserServiceTest {
             true);
 
         userService.add(testUser);
+        userRepository.save(testUser);
+        Assertions.assertTrue(userService.getUserByUsername("John Doe").isPresent());
     }
 
     @Test
     void getUsersPage() {
+        User testUser = new User("John Doe", "example@gmail.com", "ROLE_USER", "1111",
+            true);
+        User testUser2 = new User("John Doe II", "example2@gmail.com", "ROLE_USER", "1111",
+            true);
+
+        userService.add(testUser);
+        userService.add(testUser2);
+
+        List<User> users = userService.getUsersPage(1,5);
+
+        Assertions.assertEquals(2, users.size());
     }
 
     @Test
